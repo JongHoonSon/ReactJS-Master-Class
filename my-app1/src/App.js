360853f3ed171f172178572f001b896ee8ea91f3
JongHoonSon <roka1684@gmail.com>
2022-04-06 01:34:37 +0900

    #2.7 Themes
    앱 초기화

--- my-app1/src/App.js
+++ my-app1/src/App.js
@@ -1,50 +1,21 @@
 import styled, { keyframes } from "styled-components";
 
+const Title = styled.h1`
+  color: tomato;
+`;
+
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const rotationAnimation = keyframes`
-  0% {
-    transform:rotate(0deg);
-    border-radius:0px;
-  }
-  50% {
-    transform: rotate(360deg);
-    border-radius:100px
-  }
-  100% {
-    transform:rotate(0deg);
-    border-radius:0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 64px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-  animation: ${rotationAnimation} 5s linear infinite;
-  ${Emoji} {
-    &:hover {
-      font-size: 128px;
-    }
-  }
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>:)</Emoji>
-      </Box>
-      <Emoji>:)</Emoji>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
