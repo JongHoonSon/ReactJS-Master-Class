88efb805caab8f00d429b5b337634a8b516b37e3
JongHoonSon <roka1684@gmail.com>
2022-04-06 01:37:50 +0900

    #2.7 Themes
    다크모드 사용법2
    
    index.js에서 ThemeProvider에 적용된 테마의 정보를 App.js가 가져와서 사용함

--- my-app1/src/App.js
+++ my-app1/src/App.js
@@ -1,7 +1,7 @@
 import styled, { keyframes } from "styled-components";
 
 const Title = styled.h1`
-  color: tomato;
+  color: ${(props) => props.theme.textColor};
 `;
 
 const Wrapper = styled.div`
@@ -10,6 +10,7 @@ const Wrapper = styled.div`
   width: 100vw;
   justify-content: center;
   align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
