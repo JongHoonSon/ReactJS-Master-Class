4caf0a77f7f42eead6f5b736806324d62e4d0dae
JongHoonSon <roka1684@gmail.com>
2022-04-07 22:35:27 +0900

    #3.5 Forms
    Form에 onSubmit 추가

--- my-app2/src/App.tsx
+++ my-app2/src/App.tsx
@@ -3,18 +3,26 @@ import React, { useState } from "react";
 function App() {
   const [value, setValue] = useState("");
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setValue(event.currentTarget.value);
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+    console.log(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Hello", value);
   };
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
         <input
           value={value}
           onChange={onChange}
           type="text"
           placeholder="username"
         ></input>
-        <button></button>
+        <button>Log In</button>
       </form>
     </div>
   );
