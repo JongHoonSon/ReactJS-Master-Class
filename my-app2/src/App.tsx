855b89e1ab32d569af32ef0ef6ba75ae4db8b549
JongHoonSon <roka1684@gmail.com>
2022-04-07 22:52:05 +0900

    #3.6 Themes
    theme 의 속성을 props로 사용

--- my-app2/src/App.tsx
+++ my-app2/src/App.tsx
@@ -1,7 +1,18 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 function App() {
-  return <div></div>;
+  const Container = styled.div`
+    background-color: ${(props) => props.theme.bgColor};
+  `;
+  const H1 = styled.h1`
+    color: ${(props) => props.theme.textColor};
+  `;
+  return (
+    <Container>
+      <H1>protected</H1>
+    </Container>
+  );
 }
 
 export default App;
