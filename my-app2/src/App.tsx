277db26bab37c65cdf4635be6490a0b77ab39351
JongHoonSon <roka1684@gmail.com>
2022-04-07 22:26:12 +0900

    #3.5 Forms
    TypeScript 에서는 input 태그에서 발생하는 event는
    React.FormEvent<HTMLInputElement>를 타입으로 붙혀야하고,
    또한 TypeScript 에서는 event.target 대신 event.currentTarget을 사용한다.

--- my-app2/src/App.tsx
+++ my-app2/src/App.tsx
@@ -1,5 +1,23 @@
+import React, { useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setValue(event.currentTarget.value);
+  };
+  return (
+    <div>
+      <form>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        ></input>
+        <button></button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
