2ced3d2d583af4111de173ef0f7975dccf231b06
JongHoonSon <roka1684@gmail.com>
2022-04-07 23:06:17 +0900

    #4.0 Setup
    앱 초기화

--- my-app2/src/App.tsx
+++ my-app2/src/App.tsx
@@ -1,18 +1,5 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-
 function App() {
-  const Container = styled.div`
-    background-color: ${(props) => props.theme.bgColor};
-  `;
-  const H1 = styled.h1`
-    color: ${(props) => props.theme.textColor};
-  `;
-  return (
-    <Container>
-      <H1>protected</H1>
-    </Container>
-  );
+  return <div></div>;
 }
 
 export default App;
