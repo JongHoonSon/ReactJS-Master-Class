1812fdb90ee3bb514ed1e5b515405589486e8f40
JongHoonSon <roka1684@gmail.com>
2022-04-07 22:46:16 +0900

    #3.6 Themes
    앱 초기화

--- my-app2/src/App.tsx
+++ my-app2/src/App.tsx
@@ -1,31 +1,7 @@
 import React, { useState } from "react";
 
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-    console.log(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("Hello", value);
-  };
-  return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        ></input>
-        <button>Log In</button>
-      </form>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
