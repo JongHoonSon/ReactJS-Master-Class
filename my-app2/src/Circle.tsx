4fee8cef3907e77caa42f532dad61c81e5cabdd1
JongHoonSon <roka1684@gmail.com>
2022-04-07 22:01:04 +0900

    #3.4 State
    TypeScript에서 useState를 사용하면 useState가 갖는 초기 인자 값의 타입으로 counter의 타입을 자동으로 설정함

--- my-app2/src/Circle.tsx
+++ my-app2/src/Circle.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 interface ContainerProps {
@@ -20,6 +21,7 @@ interface CircleProp {
 }
 
 function Circle({ bgColor, borderColor, text = "default text" }: CircleProp) {
+  const [counter, setCounter] = useState(1);
   return (
     <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
       {text}
