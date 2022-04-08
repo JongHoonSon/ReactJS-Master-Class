b7a852eeeeb906961b5b35cb65b6ed95b06bb7f6
JongHoonSon <roka1684@gmail.com>
2022-04-08 18:48:45 +0900

    #4.15 Final Touches
    현재 페이지의 head를 수정할 수 있는 react helmet 사용

--- my-app2/src/routes/Coins.tsx
+++ my-app2/src/routes/Coins.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
 import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
@@ -67,6 +68,9 @@ function Coins() {
   const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins);
   return (
     <Container>
+      <Helmet>
+        <title>Coins</title>
+      </Helmet>
       <Header>
         <Title>Coins</Title>
       </Header>
