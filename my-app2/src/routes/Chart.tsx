02d2328aab098ed1bbbfd0e9e341112bc4b74263
JongHoonSon <roka1684@gmail.com>
2022-04-08 17:51:08 +0900

    #4.13 Price Chart part Two
    fetch 해온 데이터의 타입을 알려주는 interface 설정,
    데이터의 갯수가 많아 배열 형태로 들어오므로 [] 까지 넣음

--- my-app2/src/routes/Chart.tsx
+++ my-app2/src/routes/Chart.tsx
@@ -5,8 +5,19 @@ interface ChartProps {
   coinId: string;
 }
 
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
+
 function Chart({ coinId }: ChartProps) {
-  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId)
   );
   return <h1>{coinId} from Chart</h1>;
