534b3f5029b458ed6a244d65054cc01ef379ccb8
JongHoonSon <roka1684@gmail.com>
2022-04-12 20:53:57 +0900

    #5.1 Dark Mode part Two
    isDark의 상태값을 변경시키는 toggleDark 버튼을 다른 곳으로 이동하려고 할 경우
    
    3. Coins에서 toogleDark의 속성을 정의한 ICoinsProps를 만들고 toggleDark를 받아서 사용

--- my-app2/src/routes/Coins.tsx
+++ my-app2/src/routes/Coins.tsx
@@ -65,7 +65,11 @@ interface ICoin {
   type: string;
 }
 
-function Coins() {
+interface ICoinsProps {
+  toggleDark: () => void;
+}
+
+function Coins({ toggleDark }: ICoinsProps) {
   const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins);
 
   return (
