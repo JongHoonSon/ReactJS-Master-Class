d04159a8a5b7bdf4e58d79cab60477c9106a9eef
JongHoonSon <roka1684@gmail.com>
2022-04-12 20:27:21 +0900

    #5.0 Dark Mode part One
    Recoil 없이 라이트모드 / 다크모드 적용하기2
    darkTheme, lightTheme 생성

--- my-app2/src/App.tsx
+++ my-app2/src/App.tsx
@@ -1,7 +1,7 @@
 import { createGlobalStyle, ThemeProvider } from "styled-components";
 import Router from "./Router";
 import { ReactQueryDevtools } from "react-query/devtools";
-import { theme } from "./theme";
+import { darkTheme, lightTheme } from "./theme";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -69,7 +69,7 @@ a {
 function App() {
   return (
     <>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={darkTheme}>
         <GlobalStyle />
         <Router />
         <ReactQueryDevtools />
