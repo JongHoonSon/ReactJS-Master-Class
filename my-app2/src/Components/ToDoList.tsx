6ceb22160109a81760ee81a247f4433370a832fb
JongHoonSon <roka1684@gmail.com>
2022-04-13 22:52:11 +0900

    #5.17 Selectors part Two
    select에 categoryState 연결

--- my-app2/src/Components/ToDoList.tsx
+++ my-app2/src/Components/ToDoList.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
-import { toDoSelector, toDoState } from "../atoms";
+import { categoryState, toDoSelector, toDoState } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 function ToDoList() {
   const [toDo, doing, done] = useRecoilValue(toDoSelector);
+  const [category, setCategory] = useRecoilState(categoryState);
   const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
-    console.log(event.currentTarget.value);
+    setCategory(event.currentTarget.value);
   };
+  console.log(category);
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
-      <select onInput={onInput}>
+      <select value={category} onInput={onInput}>
         <option value="To_Do">To Do</option>
         <option value="DOING">Doing</option>
         <option value="DONE">Done</option>
