db371e166acbda33294f59c30c4701f120c486f3
JongHoonSon <roka1684@gmail.com>
2022-04-13 21:35:18 +0900

    #5.13 Categories
    const name = event.currentTarget.name

--- my-app2/src/Components/ToDo.tsx
+++ my-app2/src/Components/ToDo.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { IToDo } from "../atoms";
 
-function ToDo({ text, category }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    console.log("i wanna go to : ", event.currentTarget.name);
+    const {
+      currentTarget: { name },
+    } = event;
   };
   return (
     <li>
