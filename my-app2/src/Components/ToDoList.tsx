dfef21260a37ad9cd8d4a8ab731d408da07c0dff
JongHoonSon <roka1684@gmail.com>
2022-04-13 14:13:50 +0900

    #5.11 Add To Do
    ToDoList.tsx를 Components 폴더에 넣음

--- my-app2/src/Components/ToDoList.tsx
+++ my-app2/src/Components/ToDoList.tsx
@@ -1,28 +1,4 @@
-import { useState } from "react";
-
 import { useForm } from "react-hook-form";
-
-// function ToDoList() {
-//   const [toDo, setToDo] = useState("");
-//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-//     const {
-//       currentTarget: { value },
-//     } = event;
-//     setToDo(value);
-//   };
-//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-//     event.preventDefault();
-//     console.log(toDo);
-//   };
-//   return (
-//     <div>
-//       <form onSubmit={onSubmit}>
-//         <input onChange={onChange} value={toDo} placeholder="Write a to do" />
-//         <button>Add</button>
-//       </form>
-//     </div>
-//   );
-// }
 
 interface IForm {
   email: string;
