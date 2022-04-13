eb36e85eb57b19d36d0a912bab37439bf51a722f
JongHoonSon <roka1684@gmail.com>
2022-04-13 22:51:02 +0900

    #5.17 Selectors part Two
    select 생성

--- my-app2/src/Components/ToDoList.tsx
+++ my-app2/src/Components/ToDoList.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { toDoSelector, toDoState } from "../atoms";
 import CreateToDo from "./CreateToDo";
@@ -5,32 +6,19 @@ import ToDo from "./ToDo";
 
 function ToDoList() {
   const [toDo, doing, done] = useRecoilValue(toDoSelector);
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    console.log(event.currentTarget.value);
+  };
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
+      <select onInput={onInput}>
+        <option value="To_Do">To Do</option>
+        <option value="DOING">Doing</option>
+        <option value="DONE">Done</option>
+      </select>
       <CreateToDo />
-      <h2>To Do</h2>
-      <ul>
-        {toDo.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
-      <hr />
-      <h2>Doing</h2>
-      <ul>
-        {doing.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
-      <hr />
-      <h2>Done</h2>
-      <ul>
-        {done.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
-      <hr />
     </div>
   );
 }
