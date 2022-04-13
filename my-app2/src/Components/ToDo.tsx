711cfe0381986a0984148415edefd4f9be33dca9
JongHoonSon <roka1684@gmail.com>
2022-04-13 21:29:50 +0900

    #5.13 Categories
    User가 누른 버튼을 받는 방법1
    
    익명 함수 ()가 onClick(매개변수)을 실행하게 하고,
    매개변수를 읽어 처리한다.

--- my-app2/src/Components/ToDo.tsx
+++ my-app2/src/Components/ToDo.tsx
@@ -1,12 +1,21 @@
 import { IToDo } from "../atoms";
 
 function ToDo({ text, category }: IToDo) {
+  const onClick = (clickedCategory: IToDo["category"]) => {
+    console.log("i wanna go to : ", clickedCategory);
+  };
   return (
     <li>
       <span>{text}</span>
-      {category !== "DOING" && <button>Doing</button>}
-      {category !== "TO_DO" && <button>To Do</button>}
-      {category !== "DONE" && <button>Done</button>}
+      {category !== "DOING" && (
+        <button onClick={() => onClick("DOING")}>Doing</button>
+      )}
+      {category !== "TO_DO" && (
+        <button onClick={() => onClick("TO_DO")}>To Do</button>
+      )}
+      {category !== "DONE" && (
+        <button onClick={() => onClick("DONE")}>Done</button>
+      )}
     </li>
   );
 }
