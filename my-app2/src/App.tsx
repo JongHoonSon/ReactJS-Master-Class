46182ed3e040c4564b1f0fe55051e92895ff381c
JongHoonSon <roka1684@gmail.com>
2022-04-14 16:46:36 +0900

    에러 수정

--- my-app2/src/App.tsx
+++ my-app2/src/App.tsx
@@ -1,6 +1,8 @@
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { useRecoilState } from "recoil";
 
 import styled from "styled-components";
+import { toDoState } from "./atoms";
 
 const Wrapper = styled.div`
   display: flex;
@@ -33,9 +35,8 @@ const Card = styled.div`
   background-color: ${(props) => props.theme.cardColor};
 `;
 
-const toDos = ["a", "b", "c", "d", "e", "f"];
-
 function App() {
+  const [toDos, setToDos] = useRecoilState(toDoState);
   const onDragEnd = () => {};
   return (
     <DragDropContext onDragEnd={onDragEnd}>
@@ -45,7 +46,7 @@ function App() {
             {(magic) => (
               <Board ref={magic.innerRef} {...magic.droppableProps}>
                 {toDos.map((toDo, index) => (
-                  <Draggable draggableId={toDo} index={index}>
+                  <Draggable key={index} draggableId={toDo} index={index}>
                     {(magic) => (
                       <Card
                         ref={magic.innerRef}
