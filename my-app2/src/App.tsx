7ad31e3a9e48ba7e8abc6582fc906a7eb2160e4c
JongHoonSon <roka1684@gmail.com>
2022-04-14 17:38:22 +0900

    #6.5 Reordering
    toDos배열에서 drag된 Draggable A와
    destination (A의 drag가 끝난 시점에 위치한 곳에 위치한 Draggable B)를
    서로 교환하는 방식으로 Reordering 함
    (source와 destination의 index 값과 움직인 Draggable인 draggableId 이용)

--- my-app2/src/App.tsx
+++ my-app2/src/App.tsx
@@ -1,4 +1,9 @@
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+  DropResult,
+} from "react-beautiful-dnd";
 import { useRecoilState } from "recoil";
 
 import styled from "styled-components";
@@ -37,8 +42,14 @@ const Card = styled.div`
 
 function App() {
   const [toDos, setToDos] = useRecoilState(toDoState);
-  const onDragEnd = (args: any) => {
-    console.log(args);
+  const onDragEnd = ({ source, destination, draggableId }: DropResult) => {
+    if (!destination) return;
+    setToDos((oldToDos) => {
+      const toDosCopy = [...oldToDos];
+      toDosCopy.splice(source.index, 1);
+      toDosCopy.splice(destination?.index, 0, draggableId);
+      return toDosCopy;
+    });
   };
   return (
     <DragDropContext onDragEnd={onDragEnd}>
@@ -48,7 +59,7 @@ function App() {
             {(magic) => (
               <Board ref={magic.innerRef} {...magic.droppableProps}>
                 {toDos.map((toDo, index) => (
-                  <Draggable key={index} draggableId={toDo} index={index}>
+                  <Draggable key={toDo} draggableId={toDo} index={index}>
                     {(magic) => (
                       <Card
                         ref={magic.innerRef}
