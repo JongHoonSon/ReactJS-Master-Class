221bcba0c2446edeff5c130eb4a9f7470a638072
JongHoonSon <roka1684@gmail.com>
2022-04-14 18:57:52 +0900

    #6.9 Same Board Movement
    같은 보드에서 정렬하기 기능 구현
    
    onDragEnd 발생 시 source와 destination의 droppableId를 확인하고 같을 경우
    toDoState에 들어있는 값(allToDo라고 부름)을 가져와서
    해당 값의 source.droppableId 또는 destination.droppableId (어차피 같음)의
    내용을 변경 한 후,
    변경된 내용을 반환한다.
    
    같은 보드인지 체크를 하고 진행하므로, 아직까지 다른 보드로는 이동할 수 없음

--- my-app2/src/App.tsx
+++ my-app2/src/App.tsx
@@ -1,15 +1,9 @@
-import {
-  DragDropContext,
-  Draggable,
-  Droppable,
-  DropResult,
-} from "react-beautiful-dnd";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 import { useRecoilState } from "recoil";
 
 import styled from "styled-components";
 import { toDoState } from "./atoms";
 import Board from "./Components/Board";
-import DraggableCard from "./Components/DraggableCard";
 
 const Wrapper = styled.div`
   display: flex;
@@ -30,14 +24,20 @@ const Boards = styled.div`
 
 function App() {
   const [toDos, setToDos] = useRecoilState(toDoState);
-  const onDragEnd = ({ source, destination, draggableId }: DropResult) => {
-    if (!destination) return;
-    // setToDos((oldToDos) => {
-    //   const toDosCopy = [...oldToDos];
-    //   toDosCopy.splice(source.index, 1);
-    //   toDosCopy.splice(destination?.index, 0, draggableId);
-    //   return toDosCopy;
-    // });
+  const onDragEnd = (info: DropResult) => {
+    console.log(info);
+    const { destination, draggableId, source } = info;
+    if (destination?.droppableId === source.droppableId) {
+      setToDos((allBoards) => {
+        const boardCopy = [...allBoards[source.droppableId]];
+        boardCopy.splice(source.index, 1);
+        boardCopy.splice(destination?.index, 0, draggableId);
+        return {
+          ...allBoards,
+          [source.droppableId]: boardCopy,
+        };
+      });
+    }
   };
   return (
     <DragDropContext onDragEnd={onDragEnd}>
