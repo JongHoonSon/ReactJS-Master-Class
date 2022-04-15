9edd2b657d22a12f031804a6165ebb4136606b4b
JongHoonSon <roka1684@gmail.com>
2022-04-15 16:42:58 +0900

    #7.2 Basic Animations
    motion 태그의 모든 props에는 모든 css가 들어갈 수 있음

--- my-app2/src/App.tsx
+++ my-app2/src/App.tsx
@@ -20,7 +20,7 @@ const Box = styled(motion.div)`
 function App() {
   return (
     <Wrapper>
-      <Box initial={{ scale: 0 }} animate={{ scale: 1 }} />
+      <Box initial={{ scale: 0 }} animate={{ scale: 1, rotateZ: 360 }} />
     </Wrapper>
   );
 }
