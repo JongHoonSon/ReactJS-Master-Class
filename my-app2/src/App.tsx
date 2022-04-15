d566abcb22447943e1d4c88aedf17dc6762044ac
JongHoonSon <roka1684@gmail.com>
2022-04-15 19:14:02 +0900

    #7.11 AnimatePresence
    스타일 초기화

--- my-app2/src/App.tsx
+++ my-app2/src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Wrapper = styled(motion.div)`
   height: 100vh;
@@ -9,8 +9,22 @@ const Wrapper = styled(motion.div)`
   align-items: center;
 `;
 
+const Box = styled(motion.div)`
+  width: 400px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  position: absolute;
+  top: 100px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
 function App() {
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <Box />
+    </Wrapper>
+  );
 }
 
 export default App;
