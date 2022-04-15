ce27b4cfef0acac6857ff699451a27178c96f7e1
JongHoonSon <roka1684@gmail.com>
2022-04-15 17:03:50 +0900

    #7.4 Variants part Two
    2번째 Animation 구현
    
    Circle 컴포넌트 생성 및 적용

--- my-app2/src/App.tsx
+++ my-app2/src/App.tsx
@@ -12,15 +12,31 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
   background-color: rgba(0, 0, 0, 0.2);
-  border-radius: 10px;
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Circle = styled(motion.div)`
+  width: 70px;
+  height: 70px;
+  border-radius: 35px;
+  place-self: center;
+  background-color: white;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box></Box>
+      <Box>
+        <Circle />
+        <Circle />
+        <Circle />
+        <Circle />
+      </Box>
     </Wrapper>
   );
 }
