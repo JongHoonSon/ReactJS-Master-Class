068bb739d8bf4f8170486486d27047742d4eb124
JongHoonSon <roka1684@gmail.com>
2022-04-15 18:43:43 +0900

    #7.10 SVG Animation
    6번째 Animation 구현
    
    스타일 초기화

--- my-app2/src/App.tsx
+++ my-app2/src/App.tsx
@@ -1,11 +1,5 @@
 import styled from "styled-components";
-import {
-  motion,
-  useMotionValue,
-  useTransform,
-  useViewportScroll,
-} from "framer-motion";
-import { useEffect, useRef } from "react";
+import { motion } from "framer-motion";
 
 const Wrapper = styled(motion.div)`
   height: 200vh;
@@ -15,34 +9,8 @@ const Wrapper = styled(motion.div)`
   align-items: center;
 `;
 
-const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
-  background-color: white;
-  border-radius: 40px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
-const boxVariants = {};
-
 function App() {
-  const x = useMotionValue(0);
-  const rotateZ = useTransform(x, [-800, 800], [-360, 360]); // x가 -800일 때 2, 0일 때 1, 800일 때 0을 반환함
-  const gradient = useTransform(
-    x,
-    [-800, 800],
-    [
-      "linear-gradient(135deg,rgb(0, 123, 238),rgb(4, 0, 238))",
-      "linear-gradient(135deg,rgb(0, 238, 167),rgb(222, 238, 0))",
-    ]
-  ); // x가 -800일 때 2, 0일 때 1, 800일 때 0을 반환함
-  const { scrollYProgress } = useViewportScroll();
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]); // x가 -800일 때 2, 0일 때 1, 800일 때 0을 반환함
-  return (
-    <Wrapper style={{ background: gradient }}>
-      <Box style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin></Box>
-    </Wrapper>
-  );
+  return <Wrapper></Wrapper>;
 }
 
 export default App;
