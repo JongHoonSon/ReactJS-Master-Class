f94e787c339cd0ad7193e0f3564c43a11c491e64
JongHoonSon <roka1684@gmail.com>
2022-04-15 16:45:39 +0900

    #7.2 Basic Animations
    motion 태그의 transition props의 type은 animation을 보여주는 형식을 정할 수 있음
    (기본 값 : spring, 통통 튕기듯이 움직임)

--- my-app2/src/App.tsx
+++ my-app2/src/App.tsx
@@ -21,7 +21,7 @@ function App() {
   return (
     <Wrapper>
       <Box
-        transition={{ delay: 1 }}
+        transition={{ delay: 1, type: "tween" }}
         initial={{ scale: 0 }}
         animate={{ scale: 1, rotateZ: 360 }}
       />
