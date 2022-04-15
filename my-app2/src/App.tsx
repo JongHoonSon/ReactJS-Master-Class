dc4cfeb2a638ff9839c9e7bd5dd965eb71e9999f
JongHoonSon <roka1684@gmail.com>
2022-04-15 17:22:20 +0900

    #7.4 Variants part Two
    3번째 Animation 구현
    
    모든 스타일 삭제

--- my-app2/src/App.tsx
+++ my-app2/src/App.tsx
@@ -12,7 +12,7 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: rgba(0, 0, 0, 1);
+  background-color: white;
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
