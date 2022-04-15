78f5a9464f3b88ffed29b004627bda19f0d4ce6c
JongHoonSon <roka1684@gmail.com>
2022-04-15 17:00:37 +0900

    #7.4 Variants part Two
    2번째 Animation 구현
    
    기존에 구현한 Animation 삭제

--- my-app2/src/App.tsx
+++ my-app2/src/App.tsx
@@ -12,24 +12,15 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
+  background-color: rgba(0, 0, 0, 0.2);
   border-radius: 10px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVars = {
-  start: { scale: 0 },
-  end: {
-    scale: 1,
-    rotateZ: 360,
-    transition: { delay: 1, type: "spring" },
-  },
-};
-
 function App() {
   return (
     <Wrapper>
-      <Box variants={myVars} initial="start" animate="end" />
+      <Box></Box>
     </Wrapper>
   );
 }
