099a4b0049b70687227ef512f547b226d87ca2bb
JongHoonSon <roka1684@gmail.com>
2022-04-15 17:21:56 +0900

    #7.4 Variants part Two
    3번째 Animation 구현
    
    모든 스타일 삭제

--- my-app2/src/App.tsx
+++ my-app2/src/App.tsx
@@ -12,60 +12,17 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  background-color: rgba(0, 0, 0, 0.2);
+  background-color: rgba(0, 0, 0, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  width: 70px;
-  height: 70px;
-  border-radius: 35px;
-  place-self: center;
-  background-color: white;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
-const boxVariants = {
-  start: {
-    opacity: 0,
-    scale: 0.5,
-  },
-  end: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      delay: 1,
-      delayChildren: 1.1,
-      staggerChildren: 0.1,
-    },
-  },
-};
-
-const circleVariants = {
-  start: {
-    opacity: 0,
-    scale: 0.5,
-  },
-  end: {
-    opacity: 1,
-    scale: 1,
-  },
-};
+const boxVariants = {};
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={boxVariants} initial="start" animate="end">
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-      </Box>
+      <Box></Box>
     </Wrapper>
   );
 }
