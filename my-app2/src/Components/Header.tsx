44c7fd3565ed1e7a0015877e978fc94d1f87dd1a
JongHoonSon <roka1684@gmail.com>
2022-04-16 15:01:54 +0900

    #8.1 Header part One
    기본 스타일 적용(Only CSS)

--- my-app2/src/Components/Header.tsx
+++ my-app2/src/Components/Header.tsx
@@ -1,14 +1,34 @@
 import styled from "styled-components";
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  width: 100%;
+  top: 0;
+  background-color: tomato;
+  height: 80px;
+  font-size: 12px;
+`;
 
-const Col = styled.div``;
+const Col = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
-const Logo = styled.svg``;
+const Logo = styled.svg`
+  margin-right: 50px;
+`;
 
-const Items = styled.ul``;
+const Items = styled.ul`
+  display: flex;
+  align-items: center;
+`;
 
-const Item = styled.li``;
+const Item = styled.li`
+  margin-right: 20px;
+`;
 
 function Header() {
   return (
