49be7629d8c00d6de8604f3dc186698fed62f52e
JongHoonSon <roka1684@gmail.com>
2022-04-16 17:32:31 +0900

    #8.6 Home Screen part Two
    Wrapper 컴포넌트 생성

--- my-app2/src/Routes/Home.tsx
+++ my-app2/src/Routes/Home.tsx
@@ -1,5 +1,10 @@
 import { useQuery } from "react-query";
+import styled from "styled-components";
 import { getMovies, IGetMovieResult } from "../api";
+
+const Wrapper = styled.div`
+  background-color: black;
+`;
 
 function Home() {
   const { data, isLoading } = useQuery<IGetMovieResult>(
@@ -8,7 +13,9 @@ function Home() {
   );
   console.log(data, isLoading);
   return (
-    <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}>Home</div>
+    <Wrapper style={{ backgroundColor: "whitesmoke", height: "200vh" }}>
+      Home
+    </Wrapper>
   );
 }
 
