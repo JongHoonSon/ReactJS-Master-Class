709edb5e31541c261e9ae5f59bec55b84f949fdc
JongHoonSon <roka1684@gmail.com>
2022-04-16 14:53:38 +0900

    #8.1 Header part One
    Route에 Screen 연결

--- my-app2/src/App.tsx
+++ my-app2/src/App.tsx
@@ -1,12 +1,21 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/"></Route>
-        <Route path="/tv"></Route>
-        <Route path="/search"></Route>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/tv">
+          <Tv />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
       </Switch>
     </Router>
   );
