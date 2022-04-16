85cf93178c6cf31c515bae13e1e97a378b830201
JongHoonSon <roka1684@gmail.com>
2022-04-16 17:23:21 +0900

    #8.6 Home Screen part Two
    fetch해온 데이터의 interface 정의 및 등록

--- my-app2/src/Routes/Home.tsx
+++ my-app2/src/Routes/Home.tsx
@@ -1,8 +1,11 @@
 import { useQuery } from "react-query";
-import { getMovies } from "../api";
+import { getMovies, IGetMovieResult } from "../api";
 
 function Home() {
-  const { data, isLoading } = useQuery(["moives", "nowPlaying"], getMovies);
+  const { data, isLoading } = useQuery<IGetMovieResult>(
+    ["moives", "nowPlaying"],
+    getMovies
+  );
   console.log(data, isLoading);
   return (
     <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}>Home</div>
