daeaa39d6d9dc12bc0c9a9d7cd9b60291ccc10bb
JongHoonSon <roka1684@gmail.com>
2022-04-18 18:22:17 +0900

    코드 챌린지
    2. BigMovie 개선하기
    
    Detail 컴포넌트 구현

---
+++ my-app2/src/Routes/Detail.tsx
@@ -0,0 +1,38 @@
+import { useQuery } from "react-query";
+import { useRouteMatch } from "react-router-dom";
+import styled from "styled-components";
+import { getMovieDetail, IGetMovieDetail } from "../api";
+
+const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+function Detail() {
+  const movieMatch = useRouteMatch<{ movieId: string }>(`/movies/:movieId`);
+  const { data: info, isLoading } = useQuery<IGetMovieDetail>(
+    "MovieDetail",
+    () => getMovieDetail(movieMatch?.params.movieId)
+  );
+  return (
+    <div>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <Info>
+          <h2 style={{ fontStyle: "italic" }}>{info?.tagline}</h2>
+          <ul>
+            {info?.genres.map((genre) => (
+              <li>{genre.name}</li>
+            ))}
+          </ul>
+          <h2>{`run time : ${info?.runtime}`}</h2>
+        </Info>
+      )}
+    </div>
+  );
+}
+
+export default Detail;
