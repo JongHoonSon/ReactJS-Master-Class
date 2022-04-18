b630a6b84acfcb85918149dfb43727af65d78b5e
JongHoonSon <roka1684@gmail.com>
2022-04-18 15:42:15 +0900

    코드 챌린지
    1. Slider 추가로 더 구현하기
    
    Latest Movies를 fetch하기 위한 fetcher함수 생성

--- my-app2/src/api.ts
+++ my-app2/src/api.ts
@@ -20,8 +20,14 @@ export interface IGetMovieResult {
   total_results: number;
 }
 
-export function getMovies() {
+export function getNowPlayingMovies() {
   return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+}
+
+export function getPopularMovies() {
+  return fetch(`${BASE_PATH}/movie/popular?api_key=${API_KEY}`).then(
     (response) => response.json()
   );
 }
