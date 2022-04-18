1902ee5339a834ef451ba20780b9d49d4553c7f6
JongHoonSon <roka1684@gmail.com>
2022-04-18 21:00:59 +0900

    4. Search 구현하기
    Search 페이지에서 useQuery를 이용해 getMultiSearch의 실행결과를 가져옴

--- my-app2/src/Routes/Search.tsx
+++ my-app2/src/Routes/Search.tsx
@@ -1,9 +1,16 @@
+import { useQuery } from "react-query";
 import { useLocation } from "react-router-dom";
+import { getMultiSearch, IGetMultiSearchResult } from "../api";
 
 function Search() {
   const location = useLocation();
   const keyword = new URLSearchParams(location.search).get("keyword");
   console.log(keyword);
+  const { data: searchResult, isLoading } = useQuery<IGetMultiSearchResult>(
+    "SearchResult",
+    () => getMultiSearch(keyword ? keyword : "")
+  );
+  console.log("searchResult", searchResult);
   return null;
 }
 
