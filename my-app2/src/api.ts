3ad03e355ab99904f1f92543138976617341fa52
JongHoonSon <roka1684@gmail.com>
2022-04-18 20:03:45 +0900

    fetch 해오지만, 사용하지 않는 속성 삭제

--- my-app2/src/api.ts
+++ my-app2/src/api.ts
@@ -4,20 +4,12 @@ const BASE_PATH = "https://api.themoviedb.org/3";
 interface IMovie {
   id: number;
   backdrop_path: string;
-  poster_path: string;
   title: string;
   overview: string;
 }
 
 export interface IGetMovieResult {
-  dates: {
-    maximum: string;
-    minimum: string;
-  };
-  page: number;
   results: IMovie[];
-  total_pages: number;
-  total_results: number;
 }
 
 interface IGenres {
